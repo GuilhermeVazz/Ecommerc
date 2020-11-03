54ee8d7601506efb191fe8439b91bbd79ce01994
Guilherme Vaz <62060346+GuilhermeVazz@users.noreply.github.com>
2020-11-03 13:51:42 -0300

    Add files via upload

---
+++ src/script/index.js
@@ -0,0 +1,12 @@
+var name
+
+function pegarValor(){
+    name = document.getElementById("addCarrinho").name
+    console.log(name)
+
+    if(name == "stfJanoski"){
+        console.log(1)
+    }else{
+        console.log(2)
+    }
+}
