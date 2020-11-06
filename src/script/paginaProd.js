29d1d0f995a7d6ab75c3cce175217375fb79784a
Guilherme Vaz <62060346+GuilhermeVazz@users.noreply.github.com>
2020-11-06 13:23:18 -0300

    Add files via upload

--- src/script/paginaProd.js
+++ src/script/paginaProd.js
@@ -1,12 +1,71 @@
 function pegarCor(value) {
-
-    if (value == "vermelho") {
-        document.querySelector("#exibirTenis").src = "./src/images/stefanJanoskiVermelho.jpg"
+    corTenis = value
+    if (value == "Camo") {
+        document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Camo/defaultJanoski.webp"
+        document.querySelector("#sola").src = "./src/images/tenis/stefanJanoski/Camo/solaJanoski.webp"
+        document.querySelector("#parteCima").src = "./src/images/tenis/stefanJanoski/Camo/cimaJanoski.webp"
+        document.querySelector("#lateral").src = "./src/images/tenis/stefanJanoski/Camo/lateralJanoski.webp"
+        document.querySelector("#default").src = "./src/images/tenis/stefanJanoski/Camo/defaultJanoski.webp"
     } else if (value == "azul") {
-        document.querySelector("#exibirTenis").src = "./src/images/stefanJanoskiAzul.jpg"
+        document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/stefanJanoskiAzul.jpg"
     } else if (value == "branco") {
-        document.querySelector("#exibirTenis").src = "./src/images/stefanJanoskiBranco.jpg"
-    } else {
-        document.querySelector("#exibirTenis").src = "./src/images/stefanJanoski.jpg"
+        document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/stefanJanoskiBranco.jpg"
+    } else if (value == "preto") {
+        document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/stefanJanoski.jpg"
+        document.querySelector("#sola").src = "./src/images/tenis/stefanJanoski/Preto/solaJanoski.png"
+        document.querySelector("#parteCima").src = "./src/images/tenis/stefanJanoski/Preto/partecimaJanoski.png"
+        document.querySelector("#lateral").src = "./src/images/tenis/stefanJanoski/Preto/partelateralJanoski.png"
+        document.querySelector("#default").src = "./src/images/tenis/stefanJanoski/Preto/stefanJanoski.jpg"
     }
 }
+
+function alterarImagePrincipal(id) {
+
+    if (id == "sola") {
+        document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/solaJanoski.png"
+    } else if (id == "parteCima") {
+        document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/partecimaJanoski.png"
+    } else if (id == "lateral") {
+        document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/partelateralJanoski.png"
+    } else {
+        document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/stefanJanoski.jpg"
+    }
+
+    if (corTenis == "Camo") {
+        if (id == "sola") {
+            document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Camo/solaJanoski.webp"
+        } else if (id == "parteCima") {
+            document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Camo/cimaJanoski.webp"
+        } else if (id == "lateral") {
+            document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Camo/lateralJanoski.webp"
+        } else {
+            document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Camo/defaultJanoski.webp"
+        }
+    } else if (corTenis == "preto") {
+        if (id == "sola") {
+            document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/solaJanoski.png"
+        } else if (id == "parteCima") {
+            document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/partecimaJanoski.png"
+        } else if (id == "lateral") {
+            document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/partelateralJanoski.png"
+        } else {
+            document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/stefanJanoski.jpg"
+        }
+    }
+
+
+
+
+}
+
+// function alterarImagePrincipal(id){
+//     if(id == "sola"){
+//         document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/solaJanoski.png"
+//     }else if(id == "parteCima"){
+//         document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/partecimaJanoski.png"
+//     }else if(id == "lateral"){
+//         document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/partelateralJanoski.png"
+//     }else{
+//         document.querySelector("#exibirTenis").src = "./src/images/tenis/stefanJanoski/Preto/stefanJanoski.jpg"
+//     }
+// }
